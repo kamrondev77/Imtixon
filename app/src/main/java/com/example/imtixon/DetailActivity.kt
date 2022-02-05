d7dd2fff5685f1d30eeba068d946ef982143c7c9
acer-NITRO <kamrondev2000@gmail.com>
2022-02-05 12:23:41 +0500

    dd

--- app/src/main/java/com/example/imtixon/DetailActivity.kt
+++ app/src/main/java/com/example/imtixon/DetailActivity.kt
@@ -1,11 +1,26 @@
 package com.example.imtixon
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class DetailActivity : AppCompatActivity() {
+
+    var context: Context? = null
+    var recyclerViewHor1: RecyclerView? = null
+    var recyclerViewHor2: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        initViews()
+    }
+
+    fun initViews() {
+
+        recyclerViewHor1 = findViewById(R.id.recyclerView1)
+        recyclerViewHor1
+
     }
 }
